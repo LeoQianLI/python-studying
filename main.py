137ff6464e66ed06bda7e918ddb6dc6f20b07568
LeoQianLI <leo126com@gmail.com>
2023-10-27 22:11:17 +0200

    test python studying type(str/int/float/boolean/input/print/)

---
+++ main.py
@@ -0,0 +1,49 @@
+print('i like Viva milk!!')
+print('Viva milk is good')
+
+age = 33
+print(f"i'm "+str(age)+" years old")
+print(type(age))
+
+A = 'Leo'
+score = 18.5
+print(f'my name is {A},' " i'm "+str(age)+ ' years old,' f'i got {score} on the math')
+print(type(age),type(score))
+
+is_online = True
+print(f"are you online ？ {is_online}")
+print(type(is_online))
+
+name = 'baby'
+age = 29
+score = 99.99
+engineer = True
+
+age = float(age)
+print(age)
+print(type(age))
+
+score=int(score)
+print(score)
+print(type(score))
+
+engineer = str(engineer)
+print(engineer)
+print(type(engineer))
+
+name_1 = input('please put your name:')
+print(f"your name is {name_1}")
+
+adj_1=input("please put the 1st adjective:")
+verb_1=input("please put the 1st verb:")
+noun_1=input("please put the 1st noun:")
+verb_2=input("please put the 2nd verb:")
+noun_2=input("please put the 2nd noun:")
+adj_2=input("please put the 2nd adjective:")
+print(f"Today the weather is {adj_1}, i have nothing to {verb_1}, but i {verb_2} a video on the {noun_1},now i could use a little {noun_2} code, so i feel very {adj_2}.")
+
+width=float(input("the side width of the rectangle:"))
+length=float(input("the side length of the rectangle :"))
+
+area=width*length
+print(f"the surface of rectangle is:{area} square centmeters")
